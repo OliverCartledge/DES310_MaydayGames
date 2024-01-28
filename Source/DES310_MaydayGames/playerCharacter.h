a879868a91627de6bd347e28aecbb5ddf3e69464
Oliver Cartledge <olivercartledge@yahoo.com>
2024-01-28 01:45:19 +0000

    gitpush for move update

--- Source/DES310_MaydayGames/playerCharacter.h
+++ Source/DES310_MaydayGames/playerCharacter.h
@@ -6,6 +6,13 @@
 #include "GameFramework/Character.h"
 #include "playerCharacter.generated.h"
 
+class UInputComponent;
+class USkeletalMeshComponent;
+class USceneComponent;
+class UCameraComponent;
+class UAnimMontage;
+class USoundBase;
+
 UCLASS()
 class DES310_MAYDAYGAMES_API AplayerCharacter : public ACharacter
 {
