a4d3dbf223b046f56292762e470973fc794e1a05
Harry Findlay <harryfindlay01@gmail.com>
2024-03-13 14:43:08 +0000

    polish

--- Source/DES310_MaydayGames/DES310_MaydayGames.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "DES310_MaydayGames.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DES310_MaydayGames, "DES310_MaydayGames" );
- 
